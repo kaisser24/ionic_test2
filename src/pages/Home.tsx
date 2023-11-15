7754c7db36e05ba9d8731a32f0b7a9290ebbacd9
kaisser24 <146041449+kaisser24@users.noreply.github.com>
2023-11-15 00:32:29 -0700

    Create a React app

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,25 +1,113 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Home.css';
+import {
+  IonButton,
+  IonButtons,
+  IonContent,
+  IonHeader,
+  IonIcon,
+  IonPage,
+  IonRow,
+  IonText,
+  IonTitle,
+  IonToolbar,
+  useIonAlert,
+} from "@ionic/react"
+import { useEffect, useState } from "react"
+import { BarcodeScanner } from "@capacitor-community/barcode-scanner"
+import { scanOutline, stopCircleOutline } from "ionicons/icons"
+import "./Home.css"
 
 const Home: React.FC = () => {
+  const [err, setErr] = useState<string>()
+
+  const [hideBg, setHideBg] = useState(false)
+
+  const [present] = useIonAlert()
+
+  const startScan = async () => {
+    BarcodeScanner.hideBackground()
+    setHideBg(true)
+    // make background of WebView transparent
+
+    const result = await BarcodeScanner.startScan() // start scanning and wait for a result
+
+    // if the result has content
+    if (result.hasContent) {
+      stopScan()
+      present({
+        message: result.content,
+        buttons: [
+          "Cancel",
+          { text: "Ok", handler: (d) => console.log("ok pressed") },
+        ],
+        onDidDismiss: (e) => console.log("did dismiss"),
+      })
+      console.log(result.content) // log the raw scanned content
+    }
+  }
+
+  const stopScan = () => {
+    BarcodeScanner.showBackground()
+    setHideBg(false)
+    BarcodeScanner.stopScan()
+  }
+
+  useEffect(() => {
+    const checkPermission = async () => {
+      setErr(undefined);
+      try {
+        const status = await BarcodeScanner.checkPermission({ force: true });
+
+        if (status.granted) {
+          return true;
+        }
+
+        return false;
+      } catch (error: any) {
+        // Use 'any' type for now, but ideally, you should replace it with the actual type of the error object
+        console.error(error);
+        setErr(error.message);
+      }
+    }
+
+
+    checkPermission()
+
+    return () => {}
+  }, [])
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>QRScanner</IonTitle>
+          {hideBg && (
+            <IonButtons slot="end">
+              <IonButton onClick={stopScan} color="danger">
+                <IonIcon icon={stopCircleOutline} slot="start" />
+                Stop Scan
+              </IonButton>
+            </IonButtons>
+          )}
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
+      <IonContent className={hideBg ? "hideBg" : "ion-padding"}>
+        {err && (
+          <IonRow>
+            {" "}
+            <IonText color="danger">{err}</IonText>
+          </IonRow>
+        )}
+
+        {!!!err && hideBg && <div className="scan-box"></div>}
+        {!!!err && !!!hideBg && (
+          <IonButton className="center-button" onClick={startScan}>
+            <IonIcon icon={scanOutline} slot="start" />
+            Start Scan
+          </IonButton>
+        )}
       </IonContent>
     </IonPage>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
